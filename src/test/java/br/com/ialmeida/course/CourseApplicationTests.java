f5380f659904287bc5a136feef3772dedb843ef5
Iuri Almeida <ialmeida@vertigo.com.br>
2022-01-03 09:51:23 -0300

    Project created

---
+++ src/test/java/br/com/ialmeida/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ialmeida.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
