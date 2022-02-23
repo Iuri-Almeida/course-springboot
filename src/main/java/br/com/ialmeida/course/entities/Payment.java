62fc705109cafefe089ce969263b32a48f89a37d
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-23 19:04:41 -0300

    Exception handling - findById

--- src/main/java/br/com/ialmeida/course/entities/Payment.java
+++ src/main/java/br/com/ialmeida/course/entities/Payment.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
 
-    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/br/com/ialmeida/course/services/UserService.java
+++ src/main/java/br/com/ialmeida/course/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.ialmeida.course.services;
 
 import br.com.ialmeida.course.entities.User;
 import br.com.ialmeida.course.repositories.UserRepository;
+import br.com.ialmeida.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ResourceNotFoundException((id)));
     }
 
     public User insert(User user) {
