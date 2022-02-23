484629c7de43e89f3d6d5a37f6d7d3248d5f8d8a
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-23 13:45:04 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/ialmeida/course/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.ialmeida.course.services;
+
+import br.com.ialmeida.course.entities.User;
+import br.com.ialmeida.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
