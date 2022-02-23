7706fa9f9f51d02e77f41fe899c1baeb5a9b6325
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-23 18:44:21 -0300

    User update

--- src/main/java/br/com/ialmeida/course/resources/UserResource.java
+++ src/main/java/br/com/ialmeida/course/resources/UserResource.java
@@ -43,4 +43,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable("id") Long id, @RequestBody User user) {
+		user = userService.update(id, user);
+		return ResponseEntity.ok().body(user);
+	}
+
 }
