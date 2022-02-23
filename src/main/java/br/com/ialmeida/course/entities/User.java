4effa5e894858e94db13f2648573180a31a57232
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-23 12:26:02 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/ialmeida/course/entities/User.java
+++ src/main/java/br/com/ialmeida/course/entities/User.java
@@ -2,10 +2,18 @@ package br.com.ialmeida.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -81,11 +89,8 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+			return other.id == null;
+		} else return id.equals(other.id);
 	}
 
 }
