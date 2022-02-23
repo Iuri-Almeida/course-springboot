500d312cd1b194d1c6ba7a49a35ca42519610d55
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-23 15:06:41 -0300

    OrderStatus enum

--- src/main/java/br/com/ialmeida/course/config/TestConfig.java
+++ src/main/java/br/com/ialmeida/course/config/TestConfig.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.Arrays;
 
 import br.com.ialmeida.course.entities.Order;
+import br.com.ialmeida.course.entities.enums.OrderStatus;
 import br.com.ialmeida.course.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Bob Marley", "bob@gmail.com", "966666666", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2021-02-23T13:30:56Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2021-02-25T17:02:27Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2021-02-25T22:34:11Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2021-02-23T13:30:56Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2021-02-25T17:02:27Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2021-02-25T22:34:11Z"), OrderStatus.WAITING_PAYMENT, u3);
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
